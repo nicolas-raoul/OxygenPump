3455ef3199274d2fdc5c2962f5eb38c036570536
Nicolas Raoul <nicolas.raoul@gmail.com>
2011-10-31 20:24:30 +0900

    Archive generation

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
 groovy -classpath lib/nekohtml.jar:lib/xercesImpl.jar oxygenpump.groovy
+cp license-of-the-wikicode.txt wikicode/
+zip -r wikicode.zip wikicode
+echo "Please upload wikicode.zip to http://code.google.com/p/oxygenpump/downloads (ask Nicolas Raoul to become a project committer)"
